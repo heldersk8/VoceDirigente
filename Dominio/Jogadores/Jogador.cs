61745563c1916c5029d411a409b82de70d33fc3c
Helder Rolin Pereira <heldersk8board@hotmail.com>
2015-08-21 09:38:43 -0400

    Jogador

---
+++ Dominio/Jogadores/Jogador.cs
@@ -0,0 +1,29 @@
+﻿using Dominio.Comum;
+using System;
+
+namespace Dominio.Jogadores
+{
+    public class Jogador : Entidade<Jogador>
+    {
+        public virtual string Nome { get; set; }
+        public virtual DateTime DataDeNascimento { get; set; }
+        public virtual Posicao Posicao { get; set; }
+        public virtual int NumeroDeGols { get; set; }
+        public virtual int Assistencias { get; set; }
+        public virtual int Desarmes { get; set; }
+        public virtual int Altura { get; set; }
+        public virtual bool EhCanhoto { get; set; }
+
+        public Jogador(string nome, DateTime dataDeNascimento, Posicao posicao, int numeroDeGols, int assistencias, int desarmes, int altura, bool ehCanhoto)
+        {
+            Nome = nome;
+            DataDeNascimento = dataDeNascimento;
+            Posicao = posicao;
+            NumeroDeGols = numeroDeGols;
+            Assistencias = assistencias;
+            Desarmes = desarmes;
+            Altura = altura;
+            EhCanhoto = ehCanhoto;
+        }
+    }
+}
---
+++ Dominio/Jogadores/Posicao.cs
@@ -0,0 +1,12 @@
+﻿namespace Dominio.Jogadores
+{
+    public enum Posicao
+    {
+        Goleiro = 1,
+        Zagueiro = 2,
+        LateralDireito = 3,
+        LateralEsquerdo = 4,
+        Meia = 5,
+        Atacante = 6
+    }
+}
