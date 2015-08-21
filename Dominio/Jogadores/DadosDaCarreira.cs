bfcb1aa6b23346855029ca136890fbad3053234b
Helder Rolin Pereira <heldersk8board@hotmail.com>
2015-08-21 09:50:59 -0400

    Criação dos Dados Da Carreira

---
+++ Dominio/Jogadores/DadosDaCarreira.cs
@@ -0,0 +1,37 @@
+﻿using Dominio.Comum;
+using System.Collections.Generic;
+
+namespace Dominio.Jogadores
+{
+    public class DadosDaCarreira : Entidade<DadosDaCarreira>
+    {
+        public Clubes Clube { get; set; }
+        public IList<Titulo> Titulos { get; set; }
+        public string Biografia { get; set; }
+
+    }
+
+    public class Titulo : ObjetoDeValor<Titulo>
+    {
+        protected override bool TodosOsAtributosSaoIguais(Titulo outroObjetoDeValor)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override int ObterHashCode()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public enum Clubes
+    {
+        Cruzeiro = 1,
+        Corinthians = 2,
+        Flamengo = 3,
+        Grêmio = 4,
+        Santos = 5,
+        Atlético = 6
+
+    }
+}
