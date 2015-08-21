8bf31d27b80fb74ca20cf8719d14c6c21cc60d10
Helder Rolin Pereira <heldersk8board@hotmail.com>
2015-08-21 10:03:40 -0400

    Separação das classes Dadso da Carreira

--- Dominio/Jogadores/DadosDaCarreira.cs
+++ Dominio/Jogadores/DadosDaCarreira.cs
@@ -10,28 +10,4 @@ namespace Dominio.Jogadores
         public string Biografia { get; set; }
 
     }
-
-    public class Titulo : ObjetoDeValor<Titulo>
-    {
-        protected override bool TodosOsAtributosSaoIguais(Titulo outroObjetoDeValor)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        protected override int ObterHashCode()
-        {
-            throw new System.NotImplementedException();
-        }
-    }
-
-    public enum Clubes
-    {
-        Cruzeiro = 1,
-        Corinthians = 2,
-        Flamengo = 3,
-        Grêmio = 4,
-        Santos = 5,
-        Atlético = 6
-
-    }
 }
---
+++ Dominio/Jogadores/Titulo.cs
@@ -0,0 +1,21 @@
+﻿using Dominio.Comum;
+
+namespace Dominio.Jogadores
+{
+    public class Titulo : ObjetoDeValor<Titulo>
+    {
+        public string NomeDoCampeonato { get; set; }
+        public int Ano { get; set; }
+        public int QuantidadeDeGols { get; set; }
+
+        protected override bool TodosOsAtributosSaoIguais(Titulo outroObjetoDeValor)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override int ObterHashCode()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
