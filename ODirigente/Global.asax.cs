521345ee1bec179403af50b742354db7c8586f24
sandrooyadomari <sandrofelipe@live.com>
2015-08-21 13:55:25 -0400

    Injeção de dep

--- ODirigente/Global.asax.cs
+++ ODirigente/Global.asax.cs
@@ -1,21 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Infra.DI;
+using ODirigente.Infra;
 
 namespace ODirigente
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
+        private IContainer _container;
+
         protected void Application_Start()
         {
+            ConfigurarContainer();
             AreaRegistration.RegisterAllAreas();
+
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            DefaultModelBinder.ResourceClassKey = "Mensagens";
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ConfigurarOrm();
+            ConfigurarInjecaoDeDependencia();
+        }
+
+
+        private void ConfigurarContainer()
+        {
+            _container = ConfiguradorDeDI.ObterContainer();
+        }
+
+        private void ConfigurarInjecaoDeDependencia()
+        {
+            ControllerBuilder.Current.SetControllerFactory(new DirigenteControllerFactory(_container));
+        }
+
+        private static void ConfigurarOrm()
+        {
+            ConfiguradorDeOrm.Configurar();
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            EventosWeb.RecebendoChamada();
+        }
+
+        protected void Application_EndRequest(object sender, EventArgs e)
+        {
+            EventosWeb.EncerrandoChamada();
         }
     }
 }
---
+++ ODirigente/Infra/NinjectWebAppModule.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Dominio.Repositorios;
+using Infra.DI;
+using Infra._Base.Configuracoes;
+using NHibernate;
+using Ninject.Components;
+using Ninject.Modules;
+using Ninject.Selection.Heuristics;
+
+namespace ODirigente.Infra
+{
+    public class NinjectWebAppModule : NinjectModule
+    {
+        public class CustomInjectionHeuristic : NinjectComponent, IInjectionHeuristic
+        {
+            public bool ShouldInject(MemberInfo member)
+            {
+                return member.IsDefined(typeof(PropertyInjection), true);
+            }
+        }
+
+        public override void Load()
+        {
+            RegistrarTodosOsAssemblies();
+            SpecificBindings();
+        }
+
+        protected virtual void RegistrarTodosOsAssemblies()
+        {
+            RegistrarAssembly(typeof(Configurador));
+            RegistrarAssembly(typeof(MvcApplication));
+            RegistrarAssembly(typeof(IJogadorRepositorio));
+        }
+
+        protected virtual void SpecificBindings()
+        {
+            Kernel.Bind<ISession>().ToMethod(x => Contexto.Sessao);
+        }
+
+
+        protected virtual void RegistrarAssembly(Type type)
+        {
+            var assembly = Assembly.GetAssembly(type);
+
+            var classes = from t in assembly.GetTypes()
+                          where t.IsPublic && !t.IsAbstract && t.IsClass
+                          select t;
+
+            foreach (var @class in classes)
+            {
+                foreach (var @interface in @class.GetInterfaces())
+                    Kernel.Bind(@interface).To(@class);
+
+                if (@class.BaseType != null && @class.BaseType.IsAbstract)
+                    Kernel.Bind(@class.BaseType).To(@class);
+            }
+        }
+    }
+
+}
