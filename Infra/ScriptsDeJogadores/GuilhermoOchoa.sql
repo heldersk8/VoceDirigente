d690bc54c80f299a8ed67606238ed0c59b60b725
Jorge Silva <jorge.silva.msbr@gmail.com>
2015-08-21 13:36:43 -0400

    GuilhermoOchoa

---
+++ Infra/ScriptsDeJogadores/GuilhermoOchoa.sql
@@ -0,0 +1,24 @@
+INSERT INTO Jogador (Nome, DataDeNascimento, Posicao, NumeroDeGols, Desarmes, Assistencias, Altura, EhCanhoto)
+VALUES('Francisco Guillermo Ochoa Magaña', '1985-07-13 00:00:00.998', 1, 0, 0, 0, 183000, 1)
+GO
+INSERT INTO dadosDaCarreira(Biografia,ClubeAtual,idjogador) VALUES 
+('Francisco Guillermo Ochoa Magaña (Guadalajara, 13 de julho de 1985) é um futebolista mexicano que atua como goleiro. Defende o Málaga. Ochoa foi revelado no América onde atuou entre 2004 e 2011, quando transferiu-se ao clube francês AC Ajaccio. Em 1 de agosto de 2014 assinou contrato por três temporadas com o Málaga.', 12, SCOPE_IDENTITY())
+
+GO
+DECLARE @dadosDaCarreira int
+set @dadosDaCarreira = (select top 1 id from dadosdacarreira order by id desc)
+
+INSERT INTO Titulo (ano,NomeDoCampeonato,QuantidadeDeGols,IdDadosDaCarreira) VALUES
+(2008,'Torneo Olímpico de Fútbol de 2008', 0, @dadosDaCarreira)
+INSERT INTO Titulo (ano,NomeDoCampeonato,QuantidadeDeGols,IdDadosDaCarreira) VALUES
+(2010,'Eliminatorias Sudamericanas 2010', 2, @dadosDaCarreira)
+INSERT INTO Titulo (ano,NomeDoCampeonato,QuantidadeDeGols,IdDadosDaCarreira) VALUES
+(2010,'Copa Mundial de la FIFA 2010', 6, @dadosDaCarreira)
+INSERT INTO Titulo (ano,NomeDoCampeonato,QuantidadeDeGols,IdDadosDaCarreira) VALUES
+(2011,'Copa América 2011', 2, @dadosDaCarreira)
+INSERT INTO Titulo (ano,NomeDoCampeonato,QuantidadeDeGols,IdDadosDaCarreira) VALUES
+(2014,'Eliminatorias Sudamericanas 2014', 12, @dadosDaCarreira)
+INSERT INTO Titulo (ano,NomeDoCampeonato,QuantidadeDeGols,IdDadosDaCarreira) VALUES
+(2014,'Copa Mundial de la FIFA 2014', 4, @dadosDaCarreira)
+INSERT INTO Titulo (ano,NomeDoCampeonato,QuantidadeDeGols,IdDadosDaCarreira) VALUES
+(2015,'Copa América 2015', 3, @dadosDaCarreira)
