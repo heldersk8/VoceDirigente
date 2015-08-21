6a8c1ec9f45c8053cedf2a30258a269c2a02971a
sandrooyadomari <sandrofelipe@live.com>
2015-08-21 18:41:59 -0400

    rawr

--- ODirigente/App_Start/FilterConfig.cs
+++ ODirigente/App_Start/FilterConfig.cs
@@ -1,5 +1,5 @@
-﻿using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using ODirigente.Infra;
 
 namespace ODirigente
 {
@@ -7,6 +7,7 @@ namespace ODirigente
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
+            filters.Add(new UnitOfWorkAttribute());
             filters.Add(new HandleErrorAttribute());
         }
     }
