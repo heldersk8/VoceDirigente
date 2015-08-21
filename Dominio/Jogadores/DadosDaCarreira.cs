dfbe018e12ecd1a67d94341ab4620f7a37d2148b
Helder Rolin Pereira <heldersk8board@hotmail.com>
2015-08-21 10:09:49 -0400

    Criacao do construtor

--- Dominio/Jogadores/DadosDaCarreira.cs
+++ Dominio/Jogadores/DadosDaCarreira.cs
@@ -5,9 +5,15 @@ namespace Dominio.Jogadores
 {
     public class DadosDaCarreira : Entidade<DadosDaCarreira>
     {
-        public Clubes Clube { get; set; }
-        public IList<Titulo> Titulos { get; set; }
+        public Clubes ClubeAtual { get; set; }
+        public List<Titulo> Titulos { get; set; }
         public string Biografia { get; set; }
 
+        public DadosDaCarreira(Clubes clubeAtual, List<Titulo> titulos, string biografia)
+        {
+            ClubeAtual = clubeAtual;
+            Titulos = titulos;
+            Biografia = biografia;
+        }
     }
 }
