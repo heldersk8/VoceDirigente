8b89fdd85a6c3a13cf91afbbcaeff335373f88c8
sandrooyadomari <sandrofelipe@live.com>
2015-08-21 23:05:59 -0400

    Primeiroooo teste e utilizando FluentBuilder

---
+++ Dominio.Testes/Jogadores/JogadorTeste.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Dominio.Doacoes;
+using Dominio.Jogadores;
+using Nosbor.FluentBuilder.Lib;
+using NUnit.Framework;
+
+namespace RecrutaZero.Dominio.Testes.Jogadores
+{
+    [TestFixture]
+    public class JogadorTeste
+    {
+        [Test]
+        public void DeveEfetuarUmaDoacaoParaOJogador()
+        {
+            var doacao = FluentBuilder<Doacao>.New().Build();
+            var jogador = FluentBuilder<Jogador>.New().Build();
+
+            jogador.Efetuar(doacao);
+
+            Assert.IsTrue(jogador.Doacoes.Any(doa => doa == doacao));
+        }
+    }
+}
